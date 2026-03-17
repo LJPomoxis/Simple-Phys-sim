3ca7c3e531c8b3680c0530b19eec1c64cfcdfa14
LJPomoxis <lukeottojack@gmail.com>
2026-03-16 18:14:15 -0600

    Initial commit for Simple Physics Simulator

---
+++ README.md
@@ -0,0 +1,14 @@
+# Basic edge collsion physics program
+A very rudimentary implementation to model the physics of objects colliding with edges.
+
+## features
+- Dynamic object system: objects are handled through data structures and a vector to be able to adjust properties and the number of objects.
+
+- Basic edge collision with the edges of the screen.
+
+## Roadmap
+[] Adjust edge detection to be more dynamic using points and normals to determine collisions.
+
+[] Allow for loading of json file to create static objects in scene that physics objects can interact with.
+
+[] Update data structure with new structs to allow for more dynamism.
---
+++ main.cpp
@@ -0,0 +1,225 @@
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <random>
+
+// look at vectors and normals so that we can do fucked up math
+
+const int HEIGHT = 1080;
+const int WIDTH = 1920;
+const float RESISTANCE = 0.0025f;
+const float GRAVITY = 0.005f;
+const int NUMBER_BALLS = 25 - 1;
+
+std::random_device rd;
+std::mt19937 gen(rd());
+
+std::uniform_int_distribution<> posX(0, WIDTH);
+std::uniform_int_distribution<> randRad(10, 40);
+std::uniform_int_distribution<> coin(0, 1);
+std::uniform_int_distribution<> randHardness(20, 100);
+std::uniform_int_distribution<> randDensity(5, 20);
+std::uniform_int_distribution<> velocityMod(10, 35);
+
+class Ball {
+private:
+    const float SPEED = 5.f;
+    float initialX = 1.f;
+
+    float radius;
+    float outline;
+    float density;
+    float weight;
+
+    float xREdge;
+    float xLEdge;
+    float yREdge;
+    float yLEdge;
+
+    sf::CircleShape shape;
+    float xPos;
+    float yPos;
+    float xSpeed;
+    float ySpeed;
+    float hardness;
+
+    float check_X_Edge(float speed);
+    float check_Y_Edge(float speed);
+    float check_X_Res(float speed);
+    float check_Y_Res(float speed);
+    void set_Speed(float speedMod);
+
+    float get_X_Pos() { return xPos; }
+    float get_Y_Pos() { return yPos; }
+    float get_Rad() { return radius; }
+public:
+    Ball(float xMod, float radIn, float densityIn, float hardnessIn, float speedMod) : shape(radIn - (radIn/4.f)) {
+        radius = radIn;
+        outline = radius/4.f;
+        density = densityIn;
+        weight = density*radius;
+        hardness = hardnessIn;
+
+        xREdge = WIDTH - radius;
+        yREdge = HEIGHT - radius;
+        xLEdge = radius;
+        yLEdge = radius;
+        if (xMod < radius) {
+            xMod = radius;
+        } else if (xMod > (WIDTH - radius)) {
+            xMod = WIDTH - radius;
+        }
+        xPos = xMod;
+        yPos = radius * 2.f;
+        shape.setOrigin({(radius - outline), (radius - outline)});
+        shape.setFillColor(sf::Color(150, 50, 250));
+
+        shape.setOutlineThickness(outline);
+        shape.setOutlineColor(sf::Color(250, 150, 100));
+        set_Speed(speedMod);
+    }
+
+    void set_Ball_Pos();
+    void set_Ball_Pos(float x);
+
+    void update();
+    void draw(sf::RenderWindow& window);
+};
+
+float get_Velocity_Mod() {
+    return float(velocityMod(gen)) / 10.f;
+}
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode({WIDTH, HEIGHT}), "My window");
+    window.setFramerateLimit(60);
+    window.setVerticalSyncEnabled(true);
+
+    std::vector<Ball> balls;
+    for (int i=0; i < NUMBER_BALLS; i++) {
+        balls.emplace_back(posX(gen), randRad(gen), randDensity(gen), randHardness(gen), get_Velocity_Mod());
+    }
+
+    const auto on_Close = [&window](const sf::Event::Closed&) {
+        window.close();
+    };
+
+    const auto on_Key_Pressed = [&window, &balls](const sf::Event::KeyPressed& keyPressed) {
+        if (keyPressed.scancode == sf::Keyboard::Scancode::Escape)
+            window.close();
+
+        if (keyPressed.scancode == sf::Keyboard::Scancode::R)
+        while (!balls.empty()) {
+            balls.pop_back();
+        }
+        for (int i=0; i < NUMBER_BALLS; i++) {
+            balls.emplace_back(posX(gen), randRad(gen), randDensity(gen), randHardness(gen), get_Velocity_Mod());
+        }
+        for (Ball& ball : balls) {
+            ball.set_Ball_Pos(posX(gen));
+        }
+    };
+
+    while (window.isOpen()) {
+        // check all the window's events that were triggered since the last iteration of the loop
+        window.handleEvents(on_Key_Pressed, on_Close);
+        window.clear(sf::Color::Black);
+
+        for (Ball& ball : balls) {
+            ball.update();
+            ball.draw(window);
+        }
+
+        // end the current frame
+        window.display();
+    }
+}
+
+float Ball::check_X_Edge(float currentSpeed) {
+    if(xPos > xREdge) {
+        xPos = xREdge;
+        currentSpeed -= (RESISTANCE*hardness);
+        return currentSpeed *= -1;
+    } else if(xPos < xLEdge) {
+        xPos = xLEdge;
+        currentSpeed += (RESISTANCE*hardness);
+        return currentSpeed *= -1;
+    }
+    return currentSpeed;
+}
+
+float Ball::check_Y_Edge(float currentSpeed) {
+    if(yPos > yREdge) {
+        yPos = yREdge;
+        currentSpeed -= (RESISTANCE*hardness);
+        return currentSpeed *= -1;
+    } else if(yPos < yLEdge) {
+        yPos = yLEdge;
+        currentSpeed += (RESISTANCE*hardness);
+        return currentSpeed *= -1;
+    }
+    return currentSpeed;
+}
+
+float Ball::check_X_Res(float currentSpeed) {
+    if(currentSpeed < 0) {
+        return currentSpeed += GRAVITY;
+    }
+    
+    return currentSpeed -= GRAVITY;
+}
+
+float Ball::check_Y_Res(float speed) {
+    speed += (GRAVITY*weight);
+    if(speed < 0) {
+        return speed += (GRAVITY*RESISTANCE);
+    }
+
+    return speed;
+}
+
+void Ball::set_Speed(float speedMod) {
+    if (coin(gen)) initialX *= -1;
+
+    xSpeed = SPEED + speedMod;
+    xSpeed *= initialX;
+    ySpeed = SPEED;
+}
+
+void Ball::set_Ball_Pos() {
+    xPos = 0.f;
+    yPos = radius * 4.f;
+    set_Speed(get_Velocity_Mod());
+    shape.setPosition({xPos, yPos});
+}
+
+void Ball::set_Ball_Pos(float x) {
+    if (x > (WIDTH - radius)) {
+        x = WIDTH - radius;
+    } else if (x < radius) {
+        x = radius;
+    }
+    xPos = x;
+    yPos = radius * 2.f;
+    set_Speed(get_Velocity_Mod());
+    shape.setPosition({xPos, yPos});
+}
+
+void Ball::update() {
+    xPos += xSpeed;
+    yPos += ySpeed;
+
+    xSpeed = check_X_Edge(xSpeed);
+    ySpeed = check_Y_Edge(ySpeed);
+
+    ySpeed = check_Y_Res(ySpeed);
+    xSpeed = check_X_Res(xSpeed);
+
+    shape.setPosition({xPos, yPos});
+}
+
+void Ball::draw(sf::RenderWindow& window) {
+    window.draw(shape);
+}
